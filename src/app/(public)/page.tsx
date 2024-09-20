f4e51862b0daec7948a51914dd926ed8daeabe1e
MaryamJami1 <maryamchouhan373@gmail.com>
2024-09-20 14:12:47 +0500

    3rd class

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,14 +3,13 @@ import Link from "next/link";
 function Page() {
   return (
     <div>
-        <h1>Home Page</h1>
-         <Link href= "/">Home</Link>
-      <br/>
+      <h1>Home Page</h1>
+      <Link href="/">Home</Link>
+      <br />
       <Link href="/about">About</Link>
-      <br/>
-   <Link href="/contact">Contact</Link>
-    
-     
+      <br />
+      <Link href="/contact">Contact</Link>
+
     </div>
   )
 }
