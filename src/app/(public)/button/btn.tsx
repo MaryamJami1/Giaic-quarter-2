3404238b4173ed5a00c2dcc2825c9bc4e6503c45
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-18 10:50:16 +0500

    class-7

--- src/app/(public)/button/btn.tsx
+++ src/app/(public)/button/btn.tsx
@@ -1,12 +1,16 @@
+"use client"
 import React from 'react'
 
 interface Iprops{
     text:string
 }
 export default function Button(props:Iprops) {
+    function handleClick(){
+        alert("button click")
+    }
   return (
     <div>
-      <button className='bg-blue-600 text-white py-2 px-4 mt-10 rounded-md font-bold'>{props.text}</button>
+      <button onClick={handleClick} className='bg-blue-600 text-white py-2 px-4 mt-10 rounded-md font-bold'>{props.text}</button>
     </div>
   )
 }
